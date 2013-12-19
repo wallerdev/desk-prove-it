65df800908af1b3cf402895a44c4382e4b8cde5b
Edward Waller <edward@wallerdev.com>
2013-12-19 16:08:19 -0500

    Updated display. Set homepage to cases#index.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ DeskProveIt::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'cases#index'
   
   resources :cases, only: [:index]
   resources :labels, only: [:index]
--- spec/features/menu_spec.rb
+++ spec/features/menu_spec.rb
@@ -2,9 +2,8 @@ require 'spec_helper'
 
 feature 'Menu' do
   scenario 'User views cases' do
-    pending
-    
     visit root_path
-    click_on ''
+    click_on 'Cases'
+    expect(page).to have_css 'table#cases'
   end
 end
