059f4386c63aa023192be222b090fe194d764c4e
Edward Waller <edward@wallerdev.com>
2013-12-19 13:51:49 -0500

    Added getting labels to DeskAPI and changed specs to be more specific.

--- lib/desk_api.rb
+++ lib/desk_api.rb
@@ -17,4 +17,21 @@ class DeskAPI
 
     JSON.parse(access_token.get("#{SITE}/api/v2/cases").body)
   end
+  
+  def labels
+    consumer = OAuth::Consumer.new(
+      ENV['DESK_API_KEY'],
+      ENV['DESK_API_SECRET'],
+      :site => SITE,
+      :scheme => :header
+    )
+
+    access_token = OAuth::AccessToken.from_hash(
+      consumer, 
+      :oauth_token => ENV['DESK_API_OAUTH_TOKEN'], 
+      :oauth_token_secret => ENV['DESK_API_OAUTH_SECRET']
+    )
+
+    JSON.parse(access_token.get("#{SITE}/api/v2/labels").body)
+  end
 end
--- spec/desk_api/desk_api_spec.rb
+++ spec/desk_api/desk_api_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 require 'desk_api'
 
-describe DeskAPI do
+describe DeskAPI, '#cases' do
   it "should return a list of cases" do
     api = DeskAPI.new
-    expect(api.cases).to have_key('total_entries')
+    expect(api.cases['_links']['self']['href']).to match('cases')
+  end
+end
+
+describe DeskAPI, '#labels' do
+  it "should return a list of labels" do
+    api = DeskAPI.new
+    expect(api.labels['_links']['self']['href']).to match('labels')
   end
 end
