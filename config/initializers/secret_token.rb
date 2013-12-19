ccb2d8df4734544f8db717d9f799af26938f9df0
Edward Waller <edward@wallerdev.com>
2013-12-19 12:56:10 -0500

    Initial Rails app.
    Added database.yml to gitignore since this will be a public repo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeskProveIt::Application.config.secret_key_base = '43778d865105f111f83a32175879c816cc214e36e66a6a5638d43b46c169c5d86e364823c7285beea5a8d941f96ae0cfd4ab15f66ea9d96c3e3d9ededae8fc4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeskProveIt::Application.config.session_store :cookie_store, key: '_desk_prove_it_session'
