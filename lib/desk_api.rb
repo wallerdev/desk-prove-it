e959d4e79c8e0668f093dc33ca96ee51fec10634
Edward Waller <edward@wallerdev.com>
2013-12-19 13:55:32 -0500

    Refactored DeskAPI class to combine api requesting code.

--- lib/desk_api.rb
+++ lib/desk_api.rb
@@ -1,37 +1,32 @@
 class DeskAPI
   SITE = "https://wallerdev.desk.com"
   
-  def cases
-    consumer = OAuth::Consumer.new(
+  def initialize
+    @consumer = OAuth::Consumer.new(
       ENV['DESK_API_KEY'],
       ENV['DESK_API_SECRET'],
       :site => SITE,
       :scheme => :header
     )
-
-    access_token = OAuth::AccessToken.from_hash(
-      consumer, 
+    
+    @access_token = OAuth::AccessToken.from_hash(
+      @consumer, 
       :oauth_token => ENV['DESK_API_OAUTH_TOKEN'], 
       :oauth_token_secret => ENV['DESK_API_OAUTH_SECRET']
     )
-
-    JSON.parse(access_token.get("#{SITE}/api/v2/cases").body)
+  end
+  
+  def cases
+    request_json('/api/v2/cases')
   end
   
   def labels
-    consumer = OAuth::Consumer.new(
-      ENV['DESK_API_KEY'],
-      ENV['DESK_API_SECRET'],
-      :site => SITE,
-      :scheme => :header
-    )
-
-    access_token = OAuth::AccessToken.from_hash(
-      consumer, 
-      :oauth_token => ENV['DESK_API_OAUTH_TOKEN'], 
-      :oauth_token_secret => ENV['DESK_API_OAUTH_SECRET']
-    )
-
-    JSON.parse(access_token.get("#{SITE}/api/v2/labels").body)
+    request_json('/api/v2/labels')
+  end
+  
+  private
+  
+  def request_json(url)
+    JSON.parse(@access_token.get("#{SITE}#{url}").body)
   end
 end
